a17073cbdc406c2ebe46361e47abe88e51013f6b
tamara <tamara@focus21.io>
2018-10-17 22:17:24 -0400

    Make api port an environment variable

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const express_graphql = require("express-graphql");
 const { schema } = require("./schema");
+
+const { API_PORT: port } = process.env;
 
 // Create an express server and a GraphQL endpoint
 var app = express();
@@ -11,6 +14,6 @@ app.use(
     graphiql: true
   })
 );
-app.listen(4000, () =>
-  console.log("Express GraphQL Server Now Running On localhost:4000/graphql")
+app.listen(port, () =>
+  console.log(`Express GraphQL Server Now Running On localhost:${port}/graphql`)
 );
