60f716fc5fb912b3d14c6f666c4bed3289f98a1d
tamara <tamara@focus21.io>
2018-10-24 00:30:12 -0400

    Alliyya helped

--- api/src/mutations/user.js
+++ api/src/mutations/user.js
@@ -10,7 +10,7 @@ function hashPassword(rawPassword, salt) {
 }
 
 async function registerUser(user) {
-  const {
+  let {
     email,
     password,
     first_name,
@@ -25,55 +25,42 @@ async function registerUser(user) {
     privacy_settings
   } = user;
 
+  middle_name = middle_name || null;
+  organization = organization || null;
+  linked_in = linked_in || null;
+  twitter = twitter || null;
+  facebook = facebook || null;
+  instagram = instagram || null;
+  phone_number = phone_number || null;
+
   const salt = createSalt();
   const hashedPassword = hashPassword(password, salt);
-  let queryStringStart = `
+
+  const queryString = `
     INSERT INTO doom_user
-      (first_name, last_name, email, privacy_settings, password_hash
-  `;
-  let queryStringEnd = `values (?, ?, ?, ?, ?`;
-  const vals = [first_name, last_name, email, privacy_settings, hashedPassword];
+      (first_name, last_name, email, privacy_settings, password_hash,
+        middle_name, organization, linked_in, twitter, facebook,
+        instagram, phone_number)
+      values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) RETURNING id;`;
 
-  if (middle_name) {
-    queryStringStart = `${queryStringStart}, middle_name`;
-    queryStringEnd = `${queryStringEnd}, ?`;
-    vals.push(middle_name);
-  }
-  if (organization) {
-    queryStringStart = `${queryStringStart}, organization`;
-    queryStringEnd = `${queryStringEnd}, ?`;
-    vals.push(organization);
-  }
-  if (linked_in) {
-    queryStringStart = `${queryStringStart}, linked_in`;
-    queryStringEnd = `${queryStringEnd}, ?`;
-    vals.push(linked_in);
-  }
-  if (twitter) {
-    queryStringStart = `${queryStringStart}, twitter`;
-    queryStringEnd = `${queryStringEnd}, ?`;
-    vals.push(twitter);
-  }
-  if (facebook) {
-    queryStringStart = `${queryStringStart}, facebook`;
-    queryStringEnd = `${queryStringEnd}, ?`;
-    vals.push(facebook);
-  }
-  if (instagram) {
-    queryStringStart = `${queryStringStart}, instagram`;
-    queryStringEnd = `${queryStringEnd}, ?`;
-    vals.push(instagram);
-  }
-  if (phone_number) {
-    queryStringStart = `${queryStringStart}, phone_number`;
-    queryStringEnd = `${queryStringEnd}, ?`;
-    vals.push(phone_number);
-  }
+  const vals = [
+    first_name,
+    last_name,
+    email,
+    privacy_settings,
+    hashedPassword,
+    middle_name,
+    organization,
+    linked_in,
+    twitter,
+    facebook,
+    instagram,
+    phone_number
+  ];
 
-  queryStringStart = `${queryStringStart})`;
-  queryStringEnd = `${queryStringEnd}) RETURNING id;`;
+  console.log(`${queryString}`);
 
-  const res = await db.raw(`${queryStringStart} ${queryStringEnd}`, vals);
+  const res = await db.raw(`${queryString}`, vals);
 
   const { id } = res.rows[0];
   const newUser = {
