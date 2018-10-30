465d9d12229433ac5ccb87a3c5f685aeaefcea71
bhavanthy <bhavanthy96@live.com>
2018-10-30 05:22:30 -0400

    small api connection

--- api/src/index.js
+++ api/src/index.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const express_graphql = require("express-graphql");
 const { schema } = require("./schema");
-
 const { API_PORT: port } = process.env;
+const cors = require('cors');
 
 // Create an express server and a GraphQL endpoint
 var app = express();
+app.use(cors())
 app.use(
   "/graphql",
   express_graphql({
