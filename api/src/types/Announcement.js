d709035602af3603ae2d8b1e95ea23c3cc15a819
Alliyya Mo <alliyyamo.dev@gmail.com>
2018-10-31 06:00:59 -0400

    Creating resolver to get all announcements for a user. (#42)
    closes #13

--- api/src/types/Announcement.js
+++ api/src/types/Announcement.js
@@ -1,12 +1,14 @@
 const Announcement = `
 type Announcement {
-
+  # The id of the announcement in its respective table
   id: Int!
+  # The id of the associated event/seminar
   type_id: Int!
   message: String!
   
   date_created: String!
   date_modified: String!
+  type: String
 }
 
 input AnnouncementInput {
