09dac778fa1801d435857cd80dde6c60ac7cd5f1
Alliyya Mo <alliyyamo.dev@gmail.com>
2018-10-31 02:39:28 -0400

    Nesting types for Events & Seminars (#38)
    * Start/setup
    
    * Events have been created, closes #8
    
    * Creation of seminar mutation #9
    
    * Adding necessary types to Event and Seminar, #14 and #15

--- api/src/resolvers/seminar.js
+++ api/src/resolvers/seminar.js
@@ -12,4 +12,35 @@ async function querySeminarByID(id) {
     });
 }
 
-module.exports = { querySeminarByID };
+async function querySeminarsByEventID(id, offset = null, limit = null) {
+  var queryStringEnding = "";
+  var vals = [id];
+  if (limit) {
+    queryStringEnding += "LIMIT ? ";
+    vals.push(limit);
+  }
+  if (offset) {
+    queryStringEnding += "OFFSET ? ";
+    vals.push(offset);
+  }
+
+  var queryString = "select * from Seminar where event_id = ? ";
+
+  if (queryStringEnding) {
+    queryString += queryStringEnding;
+  }
+
+  queryString += ";";
+
+  return await db
+    .raw(queryString, vals)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+}
+
+module.exports = { querySeminarByID, querySeminarsByEventID };
