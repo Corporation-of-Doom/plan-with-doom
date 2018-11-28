a1dffb11a0afb3e1b6c169f5073887fa407d7e6f
bhavanthy <bhavanthy96@live.com>
2018-11-27 23:22:27 -0500

    search user

--- frontend/src/views/pages/UserSearch.vue
+++ frontend/src/views/pages/UserSearch.vue
@@ -1,6 +1,13 @@
 <template>
   <vue-scroll class="page-dashboard">
     <div class="timeline-box card-base card-shadow--medium" style="height: auto; padding: 20px">
+      <el-input
+        placeholder="Search"
+        prefix-icon="el-icon-search"
+        v-model="searchInput"
+        @change="onSearch"
+        style="margin-bottom: 20px"
+      />
       <div style="height:20%" v-for="item in currentList" :key="item.id">
         <el-card shadow="always" style="text-align:center;padding:20px;margin-bottom:10px;height:20%" @click.native="loadUser(item.id)">
           <el-col> {{item.first_name}} {{item.middle_name}} {{item.last_name}} </el-col>
@@ -46,25 +53,16 @@ export default {
       .then(res => {
         if (res.data) {
           this.currentList = res.data.searchUsersByName.filter(item => item.id != this.user.id)
+        } else {
+          this.$message({
+            message: "Something went wrong loading the users",
+            type: "error"
+          })
         }
       })
       .catch(err => {
         console.log(err);
       });
-      // gets the total number of cards and sets the total variable for pagination
-      fetch({
-        query: `{
-          getTotalSearchResults(searchString: "") 
-        }`
-      })
-      .then(res=> {
-        if(res.data){
-          this.total = res.data.getTotalSearchResults;
-        }
-      })
-      .catch(err => {
-        console.log(err)
-      })
     },
 
     methods: {
@@ -72,83 +70,22 @@ export default {
         // seach all events and semina based on key word
         fetch({
           query: `{
-            searchByName(searchString: "${event}", limit: 10, offset: 0) {
-              ... on Event {
-                id
-                start_time
-                end_time
-                name
-                creator_id
-                event_location: location
-              }
-              ... on Seminar {
-                id
-                start_time
-                end_time
-                name
-                location
-                event_id
-              }
+            searchUsersByName(searchString: "${event}") {
+              id
+              first_name
+              middle_name
+              last_name
             }
           }`
         })
         .then(res => {
           if (res.data) {
-            this.currentList = res.data.searchByName
-            // formats each card
-            this.currentList.forEach((element,key) => {
-              element.start_time =  moment(parseInt(element.start_time,10)).format("MMMM Do YYYY, h:mm a")
-              element.end_time =  moment(parseInt(element.end_time,10)).format("MMMM Do YYYY, h:mm a")
-              // gets name of event the seminar is in
-              if(element.event_id){
-                fetch({
-                  query: `{
-                  getEventByID(id:${element.event_id}){
-                      name
-                    }
-                  }`
-                })
-                .then(nameRes => {
-                  if(nameRes.data){
-                    element.event_id = nameRes.data.getEventByID.name
-                  } else {
-                    element.event_id = "Event Name Not Found"
-                  }
-                })
-              } else {
-                // get the name of the creator of the event
-                fetch({
-                  query: `{
-                    getEventByID(id:${element.id}){
-                      organizers {
-                        id
-                        first_name
-                        middle_name
-                        last_name
-                      }
-                    }
-                  }`
-                })
-                .then(organizerRes => {
-                  if(organizerRes.data){
-                    var result = organizerRes.data.getEventByID.organizers.filter(organizer => organizer.id === element.creator_id)[0]
-                    // makes the name of creator more readable
-                    element.creator_id = null
-                    if (result.first_name) {
-                      element.creator_id = result.first_name
-                    }
-                    if (result.middle_name) {
-                      element.creator_id +=  " " + result.middle_name
-                    }
-                    if (result.last_name) {
-                      element.creator_id +=  " " + result.last_name
-                    }
-                  }
-                })
-              }
-            });
+            this.currentList = res.data.searchUsersByName.filter(item => item.id != this.user.id)
           } else {
-            this.form.error = res.errors[0].message;
+            this.$message({
+              message: "Something went wrong loading the users",
+              type: "error"
+            })
           }
         })
         .catch(err => {
